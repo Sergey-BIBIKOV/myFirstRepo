9f2826b732a78a0f235a686e6f29182a65dce98a
Sergey Bibikov <serjantb1@gmail.com>
2022-05-26 16:50:03 +0300

    Добавленеие блока в index.html

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert("hello world"); cntl+ / быстрое коментирование 
+// console.log("hello world"); // comment 1 line
+/*
+comment
+comment
+*/
+// [1, 2, 3].push();
+var myVar = 10; // переменные надо начинать с маленькой буквы , или $ , or _ 
+//var snake_case = 11;// не преветствуется 
+var camelCaseTheBestStyle = 12;
+let snake_case = 13; //let  изменяемая переменная 
+const width = 14;// константа , не меняемая переменная 
+
